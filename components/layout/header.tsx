0f195f22dcfeed57a0c7eed8b2e834058c14bcc1
21120447 <haonhat2729@gmail.com>
2024-07-20 17:24:38 +0700

    feat: test webhook notifications Discord

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
     <header className="sticky top-0 z-50 mx-auto w-full max-w-7xl p-1 md:p-4">
       <div className="flex items-center justify-between rounded-lg bg-neutral-50/50 p-2 md:p-4 dark:bg-neutral-950/30 backdrop-blur-md">
         <Logo />
+        Hello
       </div>
     </header>
   );
